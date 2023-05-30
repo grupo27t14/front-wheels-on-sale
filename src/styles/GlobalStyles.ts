ccd1ba17ad94b2271382bbe86c28e551d6c551b5
alves-leandro <l.alves85@live.com>
2023-05-30 15:07:20 -0300

    feat: Add theme style with typography

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,9 +1,52 @@
 import { createGlobalStyle } from "styled-components";
 import { Reset } from "./reset";
+import { theme } from "./theme";
+
+//precisamos definir uma fonte
 
 export const GlobalStyle = createGlobalStyle`
  ${Reset};
 :root {
-  /* Content */
+  font-family: 'Times New Roman', Times, serif;
+  font-size: 60%;
 }
+
+@media (min-width: 700px) {
+    :root {
+      font-size: 62.5%;
+    }
+  }
+  
+  * {
+    margin:0;
+    padding: 0;
+    outline:0;
+    box-sizing: border-box;
+  }
+
+  body,html{
+    width: 100vw;
+    height: 100vh;
+  }
+
+  body {
+    background: ${theme.colors.grey4};
+    color: ${theme.colors.grey0};
+    -webkit-font-smoothing: antialiased;
+
+    overflow-x: hidden;
+  }
+
+  body, input, button, textarea {
+    font-size: 1.6rem;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong{
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
 `;
+
---
+++ src/styles/theme.ts
@@ -0,0 +1,146 @@
+export const theme = {
+  colors: {
+    brand1: "#4529E6",
+    brand2: "#5126EA",
+    brand3: "#B0A6F0",
+    brand4: "#EDEAFD",
+
+    grey10: "#FDFDFD",
+    grey9: "#F8F9FA",
+    grey8: "#F1F3F5",
+    grey7: "#E9ECEF",
+    grey6: "#DEE2E6",
+    grey5: "#CED4DA",
+    grey4: "#ADB5BD",
+    grey3: "#868E96",
+    grey2: "#495057",
+    grey1: "#212529",
+    grey0: "#0B0D0D",
+
+    whiteFixed: "#ffffff",
+
+    alert1: "#CD2B31",
+    alert2: "#FDD8D8",
+    alert3: "#FFE5E5",
+    sucess1: "#18794E",
+    sucess2: "#CCEBD7",
+    sucess3: "#DDF3E4",
+
+    random1: "#E34D8C",
+    random2: "#C04277",
+    random3: "#7D2A4D",
+    random4: "#7000FF",
+    random5: "#6200E3",
+    random6: "#36007D",
+    random7: "#349974",
+    random8: "#2A7D5F",
+    random9: "#153D2E",
+    random10: "#6100FF",
+    random11: "#5700E3",
+    random12: "#30007D",
+
+  },
+
+  typography: {
+    Heading1_700: {
+      size: "4.4rem", //44px
+      weight: "700", 
+      height: "5.6rem", //56px
+    },
+    Heading2_600: {
+      size: "3.6rem", //36px
+      weight: "600",
+      height: "4.5rem", //45px
+    },
+    Heading3_600: {
+      size: "3.2rem", //32px
+      weight: "600",
+      height: "4rem", //40px
+    },
+    Heading3_500: {
+      size: "3.2rem", //32px
+      weight: "500",
+      height: "4rem", //40
+    },
+    Heading4_600: {
+      size: "2.8rem", //28px
+      weight: "600",
+      height: "3.5rem", //35px
+    },
+    Heading4_500: {
+      size: "2.8rem", //28px
+      weight: "500",
+      height: "3.5rem", //35px
+    },
+    Heading5_600: {
+      size: "2.4rem", //24px
+      weight: "600",
+      height: "3rem", //30px
+    },
+    Heading5_500: {
+      size: "2.4rem", //24px
+      weight: "500",
+      height: "3rem", //30px
+    },
+    Heading6_600: {
+      size: "2rem", //20px
+      weight: "600",
+      height: "2.5rem", //25px
+    },
+    Heading6_500: {
+      size: "2rem", //20px
+      weight: "500",
+      height: "2.5rem", //25px 
+    },
+    Heading7_600: {
+      size: "1.6rem", //16px
+      weight: "600",
+      height: "2rem", //20px
+    },
+    Heading7_500: {
+      size: "1.6rem", //16px
+      weight: "500",
+      height: "2rem", //20px
+    },
+    body1_400: {
+      size: "1.6rem", //16px
+      weight: "400",
+      height: "2.8rem", //28px
+    },
+    body1_600: {
+      size: "1.6rem", //16px
+      weight: "600",
+      height: "2.8rem", //28px
+    },
+    body2_400: {
+      size: "1.4rem", //14px
+      weight: "400",
+      height: "2.4rem", //24px
+    },
+    body2_500: {
+      size: "1.4rem", //14px
+      weight: "500",
+      height: "2.4rem", //24px
+    },
+    button_big_text: {
+      size: "1.6rem", //16px
+      weight: "600",
+      height: "0rem", //0px
+    },
+    button_medium_text: {
+      size: "1.4rem", //14px
+      weight: "600",
+      height: "0rem", //0px
+    },
+    input_placeholder: {
+      size: "1.6rem", //16px
+      weight: "400",
+      height: "0rem", //0px
+    },
+    input_label: {
+      size: "1.4rem", //14px
+      weight: "500",
+      height: "1.7rem", //17px
+    },
+  },
+};
