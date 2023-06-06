dfa30b1c4248e03049f286ecea95eaefc717e394
alves-leandro <l.alves85@live.com>
2023-06-06 09:41:10 -0300

    fix: minnor fix at global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 import { createGlobalStyle } from "styled-components";
 import styled from "styled-components";
 import { theme } from "./theme";
 
 export const GlobalStyle = createGlobalStyle`
 :root {
-  font-family: 'Inter'; // fazer o teste
+  font-family: 'Inter';
   font-size: 60%;
 }
 
@@ -40,7 +39,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   h1, h2, h3, h4, h5, h6, strong{
-    font-family: "Lexend"; // fazer o teste
+    font-family: "Lexend";
     font-weight: 500;
   }
 
@@ -128,54 +127,3 @@ export const Avatar = styled.span<{ $bg?: string }>`
     font-weight: ${theme.typography.avatar_big.weight};
   }
 `;
-=======
-import { createGlobalStyle } from "styled-components";
-import { theme } from "./theme";
-
-export const GlobalStyle = createGlobalStyle`
-:root {
-  font-family: 'Inter', sans-serif; // fazer o teste
-  font-size: 60%;
-}
-
-@media (min-width: 700px) {
-    :root {
-      font-size: 62.5%;
-    }
-  }
-  
-  * {
-    margin:0;
-    padding: 0;
-    outline:0;
-    box-sizing: border-box;
-  }
-
-  body,html{
-    width: 100vw;
-    height: 100vh;
-  }
-
-  body {
-    background: ${theme.colors.grey0};
-    color: ${theme.colors.grey10};
-    -webkit-font-smoothing: antialiased;
-
-    overflow-x: hidden;
-  }
-
-  body, input, button, textarea {
-    font-size: 1.6rem;
-  }
-
-  h1, h2, h3, h4, h5, h6, strong{
-    font-family: "Lexend"; // fazer o teste
-    font-weight: 500;
-  }
-
-  button {
-    cursor: pointer;
-  }
-`;
-
->>>>>>> ddea9eb0f841773a82cc9408e5aa74f1d3d3bbe3
