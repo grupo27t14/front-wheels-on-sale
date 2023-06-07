7dfb09670ba2dcd296e4cb998219c42571ea9baa
Diniz <juniosantos_@hotmail.com>
2023-06-07 12:15:53 -0300

    feat: add components Footer styled

---
+++ src/components/Footer/styled.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { theme } from "../../styles/theme";
+
+export const Footer = styled.footer`
+  width: 100%;
+  height: 140px;
+  display: flex;
+  align-items: center;
+  background-color: ${theme.colors.grey0};
+
+  & .footer {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  & .footer__Button {
+  }
+`;
+
+export const Box = styled.div``;
+
+export const Img = styled.img`
+  color: ${theme.colors.whiteFixed};
+`;
+
+export const Text = styled.p`
+  ${theme.typography.body2_400}
+  color: ${theme.colors.grey3};
+`;
+
+export const Link = styled.a`
+  width: 53px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  cursor: pointer;
+  color: ${theme.colors.whiteFixed};
+  background-color: ${theme.colors.grey1};
+`;
