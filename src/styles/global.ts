5ba5cc9bd915cba04d5880d5ba57fb51a96e2b3e
Diniz <juniosantos_@hotmail.com>
2023-06-07 19:02:45 -0300

    refactor: remove @media (min-width: 1536px) on StyledContainer

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -96,13 +96,7 @@ export const StyledContainer = styled.section`
 
   @media (min-width: 1280px) {
     &.container {
-      max-width: 1280px;
-    }
-  }
-
-  @media (min-width: 1536px) {
-    .container {
-      max-width: 1536px;
+      max-width: 1522px;
     }
   }
 `;
