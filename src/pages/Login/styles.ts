2be44e6668b58262782ca18f3e2966f9582e0eb6
Diniz <juniosantos_@hotmail.com>
2023-06-07 12:14:26 -0300

    refactor: pages login style LoginStyled min-height

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -3,6 +3,7 @@ import { theme } from "../../styles/theme";
 
 export const LoginStyled = styled.main`
   width: 100vw;
+  min-height: calc(100vh - 140px - 80px);
   color: ${theme.colors.grey1};
   display: flex;
   justify-content: center;
