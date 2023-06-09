7528a01a756977245ab309285a319cd334cbfa2e
stevalves <ealves1710@hotmail.com>
2023-06-09 15:47:38 -0300

    feature: Radio Button Component

--- src/pages/Register/styles.ts
+++ src/pages/Register/styles.ts
@@ -19,32 +19,6 @@ export const RegisterStyled = styled.main`
       ${theme.typography.button_big_text}
     }
 
-    .radio {
-      [type="radio"] {
-        all: unset;
-        position: absolute;
-      }
-      label {
-        ${theme.typography.button_big_text}
-        border-radius: 4px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        height: 48px;
-        cursor: pointer;
-        background-color: transparent;
-        color: ${theme.colors.grey0};
-        border: 1.5px solid ${theme.colors.grey4};
-      }
-      [type="radio"]:checked + label {
-        background-color: ${theme.colors.brand1};
-        color: ${theme.colors.whiteFixed};
-        border: 1.5px solid ${theme.colors.brand1};
-        transition: 0.3s;
-      }
-    }
-
     > {
       label {
         ${theme.typography.input_label}
