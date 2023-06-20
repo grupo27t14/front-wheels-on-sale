962f326b0678415abb2ca1b2343a4016e32c74f0
Diniz <juniosantos_@hotmail.com>
2023-06-20 12:00:57 -0300

    perf: changing the rendering logic styling the passing

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { StyledButton } from "../../styles/button";
-import { Avatar, PageStyled } from "../../styles/global";
-import { ProductsContainer, ProfileContainer } from "./style";
+import { Avatar, PageStyled, StyledContainer } from "../../styles/global";
+import { Main, ProductsContainer, ProfileContainer } from "./style";
 import { useUsers } from "../../hooks/useUser";
 import { useEffect, useState } from "react";
 import { iPaginationCars } from "../../contexts/CarContext/props";
@@ -13,7 +13,7 @@ export const Profile = () => {
   const { id } = useParams();
   const { user, getUserCars } = useUsers();
   const [cars, setCars] = useState<iPaginationCars | null>();
-  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -22,7 +22,7 @@ export const Profile = () => {
         setCars(cars);
       }
     })();
-  }, []);
+  }, [id]);
 
   const name =
     user?.is_seller && user.id === id ? user.name : cars?.results[0].user?.name;
@@ -41,59 +41,69 @@ export const Profile = () => {
   }
 
   const handleOpenModal = () => {
-    setIsModalOpen(!isModalOpen)
-  }
+    setIsModalOpen(!isModalOpen);
+  };
 
   return (
-    <PageStyled>
-      <ProfileContainer>
-        <Avatar
-          className="avatarProfileBig"
-          $bg={
-            user?.is_seller && user.id === id
-              ? user.avatar_bg
-              : cars?.results[0].user.avatar_bg
-          }
-        >
-          {nomeESobrenome}
-        </Avatar>
-        <div className="profileName">
-          <h6 className="heading6">
+    <Main>
+      <StyledContainer className="main container">
+        <ProfileContainer>
+          <Avatar
+            className="avatarProfileBig"
+            $bg={
+              user?.is_seller && user.id === id
+                ? user.avatar_bg
+                : cars?.results[0].user.avatar_bg
+            }
+          >
+            {nomeESobrenome}
+          </Avatar>
+          <div className="profileName">
+            <h6 className="heading6">
+              {user?.is_seller && user.id === id
+                ? user.name
+                : cars?.results[0].user.name}
+            </h6>
+            <span className="sellerTag">Anunciante</span>
+          </div>
+          <p>
             {user?.is_seller && user.id === id
-              ? user.name
-              : cars?.results[0].user.name}
-          </h6>
-          <span className="sellerTag">Anunciante</span>
-        </div>
-        <p>
-          {user?.is_seller && user.id === id
-            ? user.personalInformation.description
-            : cars?.results[0].user.personalInformation?.description}
-        </p>
+              ? user.personalInformation.description
+              : cars?.results[0].user.personalInformation?.description}
+          </p>
 
-        {user?.is_seller && user.id === id && (
-          <StyledButton buttonstyle="outline_brand" buttonsize="fit" onClick={handleOpenModal}>
-            Criar Anúncio
-          </StyledButton>
-        )}
-      </ProfileContainer>
-      <ProductsContainer>
-        <h5 className="heading5">Anúncios</h5>
+          {user?.is_seller && user.id === id && (
+            <StyledButton
+              buttonstyle="outline_brand"
+              buttonsize="fit"
+              onClick={handleOpenModal}
+            >
+              Criar Anúncio
+            </StyledButton>
+          )}
+        </ProfileContainer>
+        <ProductsContainer>
+          <h5 className="heading5">Anúncios</h5>
 
-        <ul className="productsGrid">
-          {cars?.results.map((car) => (
-            <li key={car.id}>
-              <Card car={car} />
-            </li>
-          ))}
-        </ul>
-      </ProductsContainer>
+          <ul className="productsGrid">
+            {cars?.results.map((car) => (
+              <li className="productsGrid__item" key={car.id}>
+                <Card car={car} />
+              </li>
+            ))}
+          </ul>
+        </ProductsContainer>
+      </StyledContainer>
 
       {isModalOpen && (
         <Modal toggleModal={handleOpenModal}>
-          <NewAnnounce setIsModalOpen={setIsModalOpen} isModalOpen={isModalOpen} setCars={setCars} />
+          <NewAnnounce
+            setIsModalOpen={setIsModalOpen}
+            isModalOpen={isModalOpen}
+            setCars={setCars}
+          />
         </Modal>
       )}
-    </PageStyled>
+    </Main>
   );
 };
--- src/pages/Profile/style.ts
+++ src/pages/Profile/style.ts
@@ -1,18 +1,38 @@
 import styled from "styled-components";
 import { theme } from "../../styles/theme";
 
+export const Main = styled.main`
+  width: 100vw;
+  min-height: calc(100vh - 80px - 140px);
+  height: 100%;
+  background: linear-gradient(
+    180deg,
+    ${theme.colors.brand1} 277px,
+    ${theme.colors.grey8} 277px,
+    ${theme.colors.grey8} 100%
+  );
+
+  .main {
+    height: 100%;
+    padding-top: 7.5rem;
+  }
+`;
+
 export const ProfileContainer = styled.section`
   background-color: ${theme.colors.grey10};
 
   max-width: 1240px;
+  max-height: 406px;
   width: 100%;
-  margin: 6rem auto;
+  height: 100%;
+  margin: 0rem auto;
 
-  border-radius: .4rem;
+  border-radius: 0.4rem;
+
   display: flex;
   flex-direction: column;
-  padding: 4rem;
   gap: 2.4rem;
+  padding: 4.4rem 7rem 4.1rem 4.1rem;
 
   .heading6 {
     ${theme.typography.Heading6_600};
@@ -35,56 +55,44 @@ export const ProfileContainer = styled.section`
   }
 
   .sellerTag {
-      padding: .4rem .8rem;
-      color: ${theme.colors.brand1};
-      background-color: ${theme.colors.brand4};
-      ${theme.typography.body2_500}
-    }
-
-`
+    padding: 0.4rem 0.8rem;
+    color: ${theme.colors.brand1};
+    background-color: ${theme.colors.brand4};
+    ${theme.typography.body2_500}
+  }
+`;
 
 export const ProductsContainer = styled.section`
-  max-width: 1480px;
   width: 100%;
-  margin: 0 auto;
-
-  padding-bottom: 4rem;
-
   display: flex;
   flex-direction: column;
-  gap: 2rem;
+  gap: 6.5rem;
+
+  padding-top: 7.4rem;
 
   .heading5 {
     ${theme.typography.Heading5_600};
   }
 
   .productsGrid {
-    display: grid;   
-    grid-template-columns: repeat(8, 1fr);
-    grid-auto-rows: auto;
-    grid-column-gap: 20px;
-    grid-row-gap: 40px;
-    
-    overflow-x: auto;
-    max-width: 100%;
+    max-width: 1392px;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(312px, 1fr));
+    gap: 1.2rem;
+    margin: 0 auto;
 
-    .card {
-      width: 300px;
-      height: 350px;
-      border-radius: .8rem;
-      background-color: ${theme.colors.brand3};
-    }
-
-    @media (min-width: 700px) {    
-      grid-template-columns: repeat(2, 1fr);
-    }
-
-    @media (min-width: 1030px) {    
-      grid-template-columns: repeat(3, 1fr);
-    }
-
-    @media (min-width: 1300px) {    
-      grid-template-columns: repeat(4, 1fr);
+    @media (max-width: 768px) {
+      display: flex;
+      overflow-x: auto;
     }
   }
-`
+
+  .productsGrid__item {
+    margin-bottom: 6.3rem;
+
+    @media (max-width: 768px) {
+      margin-bottom: unset;
+    }
+  }
+`;
