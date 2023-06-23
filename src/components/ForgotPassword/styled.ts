6391ef5f5d2e8990631b33cbf492006d6d9a908e
Diniz <juniosantos_@hotmail.com>
2023-06-23 20:07:38 -0300

    feat: add ForgotPassword on components

---
+++ src/components/ForgotPassword/styled.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { theme } from "../../styles/theme";
+
+export const FormControl = styled.section`
+  width: 100%;
+
+  > form {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export const HStack = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  ${theme.typography.button_big_text}
+
+  & > button {
+    padding: 0;
+  }
+`;
