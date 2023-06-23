fea25662172abec945cb2a4378380cdd1ac4dce5
Diniz <juniosantos_@hotmail.com>
2023-06-23 01:41:02 -0300

    feat: add component EditAddress

---
+++ src/components/EditAddress/styled.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { theme } from "../../styles/theme";
+
+export const FormControl = styled.section`
+  width: 100%;
+
+  & > form {
+    width: 100%;
+    margin: 0;
+    padding-left: 0;
+    padding-right: 0;
+  }
+`;
+
+export const Flex = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+`;
+
+export const HStack = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end !important;
+  gap: 1rem;
+
+  ${theme.typography.button_big_text}
+
+  & > button {
+    width: unset;
+  }
+`;
