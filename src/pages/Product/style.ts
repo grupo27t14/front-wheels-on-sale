ca0b896bef257e70025a7b3a022010d5448295fc
alves-leandro <l.alves85@live.com>
2023-06-29 15:34:40 -0300

    fix: remove justify-content

--- src/pages/Product/style.ts
+++ src/pages/Product/style.ts
@@ -100,7 +100,7 @@ export const GalleryGrid = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  justify-content: space-between;
+  /* justify-content: space-between; */
   gap: 10px;
 
   .imgContainer {
